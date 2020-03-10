43fb5179ccd4f6cfc61dfb8dd82c3094ca1f11a5
alex <zeimedee@gmail.com>
2020-03-10 13:42:32 +0000

    webcam

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const video = document.getElementById("video");
 
+
 const startVideo = () =>{
     navigator.getUserMedia(
         {video: {}},
         stream => video.srcObject = stream,
         err => console.error(err)
     )
+
+    
 }
 
+video.className = 'anime'
 startVideo();
